319db426073e17c57fe7a1da311df5e3861d50e4
Alvaro Luque Morillo <aluqmor651@ieszaidinvergeles.org>
2025-01-03 11:57:12 +0100

    Ejercicio 4a parte 2

--- Ejercicio 4/ej4a/index.js
+++ Ejercicio 4/ej4a/index.js
@@ -5,4 +5,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const num1 = document.getElementById('num1');
     const num2 = document.getElementById('num2');
     const res = document.getElementById('resultado');
+    
+    // creo una funcion para calcular sumas y restas
+    const calcular = () => {
+        const valor1 = num1.value; // el valor del input1 sera el numero introducido
+        const valor2 = num2.value; // el valor del input2 sera el numero introducido
+        let resultado = 0;
+
+        // (se que esto da un poco de asquete pero es la forma mas simple que se me ocurria)
+        if (ope.value == 'suma') {
+            resultado = valor1 + valor2;
+        } else if (ope.value == 'resta') {
+            resultado = valor1 - valor2;
+        }
+
+        res.textContent = resultado; // el resultado sera el valor de la variable resultado
+    };
+
+    // eventos para que al meter un numero se ejecute la funcion
+    num1.addEventListener('input', calcular); 
+    num2.addEventListener('input', calcular);
+    ope.addEventListener('change', calcular); // al cambiar de operacion se ejecuta la funcion
 })
