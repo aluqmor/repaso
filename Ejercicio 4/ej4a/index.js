16683450cca40924727c4b11be9d7b661bdb46bb
Alvaro Luque Morillo <aluqmor651@ieszaidinvergeles.org>
2025-01-03 11:47:40 +0100

    Ejercicio 4a parte 1

---
+++ Ejercicio 4/ej4a/index.js
@@ -0,0 +1,8 @@
+// cuando se cargue el documento: 
+document.addEventListener('DOMContentLoaded', () => {
+    // obtengo los elementos y los guardo en constantes
+    const ope = document.getElementById('operacion'); 
+    const num1 = document.getElementById('num1');
+    const num2 = document.getElementById('num2');
+    const res = document.getElementById('resultado');
+})
